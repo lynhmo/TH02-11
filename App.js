5fad8008b0711e6b68a484f14693505d956bf49e
Lynh <chinsugaming@gmail.com>
2022-11-02 13:52:18 +0700

    first commit

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Onboarding from './components/onBoarding';
-import Landingscreen from './components/LandingScreen';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Onboarding from "./components/onBoarding";
+import Landingscreen from "./components/LandingScreen";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Onboarding'>
+      <Stack.Navigator
+        initialRouteName="Onboarding"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Onboarding" component={Onboarding} />
         <Stack.Screen name="Landingscreen" component={Landingscreen} />
       </Stack.Navigator>
-    </NavigationContainer >
-  )
+    </NavigationContainer>
+  );
 }
